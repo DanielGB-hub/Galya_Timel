e50cbac24c887aca36933b753dfe03e332e5a41d
DanielGB-hub <qwer.82@mail.ru>
2021-09-28 17:18:03 +0300

    Add models in admin.py, createsuperuser(D, 1)

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from mainapp.models import *
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(PhotoCard)
+admin.site.register(TextBlock)
+admin.site.register(ImageBlock)
